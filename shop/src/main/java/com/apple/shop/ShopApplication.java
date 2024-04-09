0c850e26cdee57acdb3e1eb3f0a1afc57c08aa34
joominse0 <sally51501512@gmail.com>
2024-04-10 01:05:21 +0900

    자바 기본 문법(변수, 함수, if, for)

--- shop/src/main/java/com/apple/shop/ShopApplication.java
+++ shop/src/main/java/com/apple/shop/ShopApplication.java
@@ -8,6 +8,12 @@ public class ShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
-	}
+		final String lover = "주민서"; //final키워드 붙이면 변경 불가능
+		final int age = 22; //type대신 var키워드 가능
+		System.out.println(lover);
+		System.out.println(age);
 
+
+
+	}
 }
