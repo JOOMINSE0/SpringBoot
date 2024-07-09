f3fde3cb47795e8b989130df3e3faa5306f4a39d
joominse0 <sally51501512@gmail.com>
2024-07-09 18:09:13 +0900

    테이블 만들기 (Entity)

---
+++ shop/src/main/java/com/apple/shop/Item.java
@@ -0,0 +1,14 @@
+package com.apple.shop;
+
+import jakarta.persistence.*;
+
+//테이블 생성
+@Entity
+public class Item {
+    //테이블에 어떤 컬럼이 들어갈지도 먼저 정해야 함
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    //@Column()으로 컬럼에 제약설정 가능
+    public String title; //변수에 public을 붙이면 다른 모든 클래스에서 문제없이 사용 가능
+    public Integer price; //20억까지 저장 가능
+}
