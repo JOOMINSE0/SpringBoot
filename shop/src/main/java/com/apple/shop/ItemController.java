599bc971847c4ee264f763e6ac7688acb911e7b5
joominse0 <sally51501512@gmail.com>
2024-07-12 00:23:14 +0900

    상품 추가기능 1 (Map 자료형)

--- shop/src/main/java/com/apple/shop/ItemController.java
+++ shop/src/main/java/com/apple/shop/ItemController.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller //api만들 수 있음
 @RequiredArgsConstructor
@@ -19,9 +23,29 @@ public class ItemController {
     String list(Model model){
         List<Item> result = itemRepository.findAll(); //List자료형으로 가져옴
         model.addAttribute("items", result);
-        var a = new Item();
-        System.out.println(a.toString()); //object 한눈에 보고싶으면 .toStrind()만들어서 쓰기
+//        var a = new Item();
+//        System.out.println(a.toString()); //object 한눈에 보고싶으면 .toStrind()만들어서 쓰기
         return "list.html";
     }
+
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam Map formData) {
+        //Map자료형(여러 데이터 한 변수에 넣고 싶을 때)
+        var test = new HashMap<>();
+        test.put("name", "ju");
+        test.put("age", "22");
+        System.out.println(test.get("name")); //Map에서 자료 하나만 뽑으려면
+        itemRepository.save();
+        return "redirect:/list";
+
+    }
+
+
+
 
 }
