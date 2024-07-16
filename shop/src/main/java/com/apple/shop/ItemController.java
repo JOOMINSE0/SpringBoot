2e2f04d30d231ac952eeeb829fcfc1bab612275d
joominse0 <sally51501512@gmail.com>
2024-07-16 22:37:53 +0900

    삭제기능 1 (AJAX, query string)

--- shop/src/main/java/com/apple/shop/ItemController.java
+++ shop/src/main/java/com/apple/shop/ItemController.java
@@ -76,4 +76,10 @@ public class ItemController {
     return "redirect:/list";
     }
 
+    @PostMapping("/test1")
+    String test1(@RequestBody Map<String, Object> body) {
+        System.out.println(body.get("name"));
+        return "redirect:/list";
+    }
+
 }
